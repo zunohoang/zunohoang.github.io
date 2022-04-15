888e3e033ee50d0b7ca1edc8f947bad8a7b3f750
Nguyễn Văn Hoàng <88831612+zunohoang@users.noreply.github.com>
2022-04-15 12:03:08 +0700

    Update myJs.js

--- webforcrushzuno/js/myJs.js
+++ webforcrushzuno/js/myJs.js
@@ -1,7 +1,7 @@
 const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa:3333",
-  text3: "Cậu yêu tớ có phải không nào ._.",
+  text3: "Hi! Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
   text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
